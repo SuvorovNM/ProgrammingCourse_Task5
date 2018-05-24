ef3f5b90ad885a4d0c3d17900593f91e587eba42
SuvorovNM <35630837+SuvorovNM@users.noreply.github.com>
2018-05-24 15:02:34 +0500

    commented

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -13,14 +13,17 @@ namespace Task5
             bool OK = true;
             int i, j,n;
             string str;
+            //Создание квадратной матрицы
             do
             {
+                //Проверка на корректность ввода
                 Console.WriteLine("Введите размерность матрицы:");
                 str = Console.ReadLine();
                 OK = Int32.TryParse(str, out n);
-            } while (!OK||n<2);
-            int[,] matr = new int[n, n];
-            int[,] newmatr = new int[n-1,n-1];
+            } while (!OK||n<2);//Пока не введено целое число>= 2
+            int[,] matr = new int[n, n];//Инициализация исходной матрицы
+            int[,] newmatr = new int[n-1,n-1];//Инициализация полученной матрицы
+            //Ввод i и j, по координатам которых нужно удалить i-тую строку и j-тый столбец
             do
             {
                 Console.WriteLine("Введите координату X:");
@@ -35,6 +38,7 @@ namespace Task5
             } while (!OK || j > n || j <= 0);
             Random rng = new Random();
             Console.WriteLine("Исходная матрица: ");
+            //Заполнение исходной матрицы случайными числами
             for (int k = 0; k < n; k++)
             { 
                 for (int t = 0; t < n; t++)
@@ -45,12 +49,15 @@ namespace Task5
                 Console.WriteLine();
             }
             Console.WriteLine("Полученная матрица: ");
+            //Новая матрица - соединенные 4 части старой, разделенные i-той строкой и j-тым столбцом
             for (int k = 0; k < i - 1; k++)
             {
+                //Левая верхняя часть
                 for (int t = 0; t < j - 1; t++)
                 {
                     newmatr[k, t] = matr[k, t];
                 }
+                //Левая нижняя часть
                 for (int t = j; t < n; t++)
                 {
                     newmatr[k,t-1] = matr[k, t];
@@ -58,15 +65,18 @@ namespace Task5
             }
             for (int k = i; k < n; k++)
             {
+                //Правая верхняя часть
                 for (int t = 0; t < j - 1; t++)
                 {
                     newmatr[k-1, t] = matr[k, t];
                 }
+                //Правая нижняя часть
                 for (int t = j; t < n; t++)
                 {
                     newmatr[k-1, t - 1] = matr[k, t];
                 }
             }
+            //Вывод полученной матрицы
             for (int k = 0; k < n-1; k++)
             {
                 for (int t = 0; t < n-1; t++)
